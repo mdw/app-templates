de4e45dd6d044d88c674733169db598cd755c3c0
Mike Ward <github@mikeward.net>
2009-02-25 13:01:10 -0500

    fixed soem typos

--- user_auth_openid_git.rb
+++ user_auth_openid_git.rb
@@ -1,5 +1,4 @@
-#base_template.rb
-# this is not intended to be used directly - it is included in git or svn versions
+# user_auth_openid_git.rb
 
 # install plugins for user authentication
 plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git', :submodule => true
@@ -17,13 +16,33 @@ rake('open_id_authentication:db:create')
 rake "db:migrate"
 
 # setup the session store
-nitializer 'session_store.rb', <<-END
+initializer 'session_store.rb', <<-END
 ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
 ActionController::Base.session_store = :active_record_store
   END
 
+# create homepage
 generate :controller, "welcome index"
 route "map.root :controller => 'welcome'"
 run "rm public/index.html"
 run "echo 'coming soon' > README"
+run "cp config/database.yml config/example_database.yml"
+
+# setup git
+git :init
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+
+file ".gitignore", <<-EOGIT
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+notes.txt
+EOGIT
+
+git :add => "."
+git :commit => "-m 'initial commit'"
+
+puts "done with setup"
 
